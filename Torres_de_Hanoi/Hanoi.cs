0ed9b59838b508f100210ce0e6b39e466be0a7c4
Moushes <adrian123van@gmail.com>
2023-03-13 16:42:59 +0100

    Version 1.2

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -10,33 +10,54 @@ namespace Torres_de_Hanoi
     {
         /*TODO: Implementar métodos*/
         public void mover_disco(Pila a, Pila b){
-            if(a.Top > b.Top && b.Top == 0){
+            /*if(a.Top > b.Top && b.Top == 0){  //si pila a es mayor que pila b y top de b es = 0
 
-                Disco numMover = new Disco();
-                numMover = a.Pop();
-                b.push(numMover);
+                Disco numMover = new Disco();  //creamos disco nuevo
+                numMover = a.Pop();            //asignamos el elementos que hemos quitado de la primera pila
+                b.push(numMover);              //metemos el nuevo elemento en la segunda pila
 ;
             }
-            else if(a.Top > b.Top && b.Top != 0){
+            else if(a.Top > b.Top && b.Top != 0){ // si pila a es mayor que pila b y top de b es distinto de 0
 
                 Disco numMover = new Disco();
                 numMover = b.Pop();
                 a.push(numMover);
 
             }
-            else if(a.Top < b.Top && a.Top == 0){
+            else if(a.Top < b.Top && a.Top == 0){ // si pila a es menor que pila b y top de a es igual a 0
                 Disco numMover = new Disco();
                 numMover = b.Pop();
                 a.push(numMover);
 
             }
-            else{
-
+            else if(a.Top < b.Top && a.Top != 0){
                 Disco numMover = new Disco();
                 numMover = a.Pop();
                 b.push(numMover);
+            }*/
+           if(a.Top == 0 || b.Top == 0){ 
+            if(a.Top < b.Top){
+                    a.push(b.Pop());
+            }
+                else{
+                    b.push(a.Pop());
+                }
+            
+            }
+            else
+            {
+                if (a.Top > b.Top)
+                {
+                    a.push(b.Pop());
+                }
+                else
+                {
+                    b.push(a.Pop());
+                }
+
             }
         }
+
         public void imprimir(Pila ini, Pila aux, Pila fin,int m){
             
             Console.Write("Movimiento " + m + "\n");
@@ -57,7 +78,7 @@ namespace Torres_de_Hanoi
         public int iterativo(int n, Pila ini, Pila aux, Pila fin)
         {
             int m = 0;
-            if (n % 2 == 0)
+            if (n % 2 == 0) //si n es par
             {
                 while (fin.Size != n)
                 {
@@ -78,13 +99,13 @@ namespace Torres_de_Hanoi
             }
             else
             {
-                while(fin.Size != n)
+                while(fin.Size != n)  //si n es impar
                 {
                     mover_disco(ini, fin);
                     m++;
                     imprimir(ini, aux, fin, m);
 
-                    if (fin.Size == n){
+                    if (fin.Size == n){ //en este movimiento se termina el bucle con los impares, mira si la pila final está llena
                         return m;
                     }
 
@@ -98,6 +119,23 @@ namespace Torres_de_Hanoi
                 }
                 return m;
             }
+        }
+        int mov = 0; // variable que se crea fuera pq al llamar recurivo de nuevo se resetea
+        public int recursivo(int n, Pila ini, Pila aux, Pila fin){
+            
+            if(n == 1){
+                mov = mov + 1;
+                mover_disco(ini, fin);
+                imprimir(ini, aux, fin, mov);
+            }
+            else{
+                recursivo(n - 1, ini, aux, fin);
+                mover_disco(ini, fin);
+                mov = mov + 1;
+                imprimir(ini, aux, fin, mov);
+                recursivo(n - 1, aux, fin, ini);
+            }
+            return mov;
         }
     }
 }
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -17,19 +17,28 @@ namespace Torres_de_Hanoi
             Pila pilaI = new Pila(userInputNum);
             Pila pilaA = new Pila();
             Pila pilaF = new Pila();
-
             int movimientos;
-            
-            //foreach (Disco disco in pilaI.Elementos)
-            //{
-            //    Console.Write(disco.Valor);
-            //}
-            
-            Hanoi hanoi = new Hanoi();
-            movimientos = hanoi.iterativo(userInputNum, pilaI, pilaA, pilaF);
-            Console.Write("Movimientos totales: " + movimientos);
-            Console.Write("\n");
 
+            Console.Write("Iterativo o recursivo?" + "\n");
+            Console.Write("i/r" + "\n");
+            String userChoice = Console.ReadLine();
+            if (userChoice == "i")
+            {
+
+                Hanoi hanoi = new Hanoi();
+                movimientos = hanoi.iterativo(userInputNum, pilaI, pilaA, pilaF);
+                Console.Write("Movimientos totales: " + movimientos);
+                Console.Write("\n");
+
+
+            }
+            if (userChoice == "r")
+            {
+                Hanoi hanoi = new Hanoi();
+                movimientos = hanoi.recursivo(userInputNum, pilaI, pilaA, pilaF);
+                Console.Write("Movimientos totales: " + movimientos);
+                Console.Write("\n");
+            }
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
